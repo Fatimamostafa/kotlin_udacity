acd5279c856ef332a49d5d091d4a5d3727d92f36
Fatima Mostafa <hn.fatimamostafa@gmail.com>
2019-02-04 15:03:51 +0600

    implemented data binding

--- app/src/main/java/com/example/udacity_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/udacity_kotlin/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.udacity_kotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.udacity_kotlin.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        binding.apply {
+            tvNickname.text = etNickname.text
+            invalidateAll()
+            etNickname.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            tvNickname.visibility = View.VISIBLE
+        }
+
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
