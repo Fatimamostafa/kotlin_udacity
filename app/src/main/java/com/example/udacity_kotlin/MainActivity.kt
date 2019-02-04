6607d58e5c8ad9c4da4fcad48a67540d7c9a3bf0
Fatima Mostafa <hn.fatimamostafa@gmail.com>
2019-02-04 17:14:53 +0600

    databinding

--- app/src/main/java/com/example/udacity_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/udacity_kotlin/MainActivity.kt
@@ -6,14 +6,19 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.example.udacity_kotlin.data.MyName
 import com.example.udacity_kotlin.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val myName : MyName = MyName("Fatima Mostafa")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
 
         binding.doneButton.setOnClickListener {
             addNickname(it)
@@ -22,13 +27,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickname(view: View) {
         binding.apply {
-            tvNickname.text = etNickname.text
+            //tvNickname.text = etNickname.text
+            myName?.nickname = etNickname.text.toString()
             invalidateAll()
             etNickname.visibility = View.GONE
             doneButton.visibility = View.GONE
             tvNickname.visibility = View.VISIBLE
         }
-
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
---
+++ app/src/main/java/com/example/udacity_kotlin/data/MyName.kt
@@ -0,0 +1,7 @@
+package com.example.udacity_kotlin.data
+
+/**
+ * Created by Fatima Mostafa on 2/4/19 3:12 PM.
+ * the.fatimamostafa@gmail.com
+ */
+data class MyName(var name: String = "", var nickname: String = "")
