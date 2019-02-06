7c91f5b8f5309fdbcb2c8b180e248d8fde1de4b0
Fatima Mostafa <hn.fatimamostafa@gmail.com>
2019-02-06 17:50:12 +0600

    navigate through fragments

--- app/src/main/java/com/example/udacity_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/udacity_kotlin/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.udacity_kotlin
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.udacity_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+    }
 }
--- app/src/main/java/com/example/udacity_kotlin/fragments/TitleFragment.kt
+++ app/src/main/java/com/example/udacity_kotlin/fragments/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.udacity_kotlin.R
 import com.example.udacity_kotlin.databinding.FragmentTitleBinding
 
@@ -15,9 +16,11 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater,
-            R.layout.fragment_game_over, container, false)
+            R.layout.fragment_title, container, false)
 
-
+        binding.playButton.setOnClickListener { view: View ->
+          view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
 
         return binding.root
     }
