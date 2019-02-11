221937daf4d0dd42933f781338f24eac60ca50bb
Fatima Mostafa <hn.fatimamostafa@gmail.com>
2019-02-11 18:22:49 +0600

    Nav drawer feature [incomplete]

--- app/src/main/java/com/example/udacity_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/udacity_kotlin/MainActivity.kt
@@ -3,16 +3,18 @@ package com.example.udacity_kotlin
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.udacity_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-
+        drawerLayout = binding.drawerLayout
         val navController = this.findNavController(R.id.nav_host_fragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
